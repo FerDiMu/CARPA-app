f84e74131de9dd092f01af689b0988ffe4827e89
Fernando Díez <fernando.diez.munoz@upm.es>
2024-04-23 10:07:56 +0200

    refactor: gitignore

--- apps/reader/src/firebase/firebaseApp.ts
+++ apps/reader/src/firebase/firebaseApp.ts
@@ -5,13 +5,8 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: ,
-  authDomain: ,
-  projectId: ,
-  storageBucket: ,
-  messagingSenderId: ,
-  appId: 
+const firebaseConfig = {
+  apiKey: "michi",
 };
 
 // Initialize Firebase
