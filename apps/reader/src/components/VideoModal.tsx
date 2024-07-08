fd7a5497499062e046da9749b34d8578fbcba85c
Fernando Díez <fernando.diez.munoz@upm.es>
2024-07-08 15:09:30 +0200

    fix: calibration video not showing in dialog

--- apps/reader/src/components/VideoModal.tsx
+++ apps/reader/src/components/VideoModal.tsx
@@ -20,21 +20,73 @@ export const VideoModal = ({
   let [isOpen, setIsOpen] = useState(true)
   let iframeRef = useRef(null)
   const vidRef = useRef<any>(null)
+
+  const onLoad = async () => {
+    console.log('Weblogger: Dialog loaded')
+    const Player = (await import('liqvid')).Player
+    const Script = (await import('liqvid')).Script
+    const Video = (await import('liqvid')).Video
+    const handlePlayVideo = () => {
+      vidRef.current!.playback.pause()
+    }
+    createRoot(
+      document.querySelector('iframe')!.contentDocument!.querySelector('main')!,
+    ).render(
+      <Player id="myPlayer" ref={vidRef} script={new Script(script)}>
+        <Video id="myVideo" style={{ aspectRatio: '16/10', width: '100%' }}>
+          <source src={mediaURL} type="video/mp4" />
+        </Video>
+        <div
+          id="button-div"
+          style={{
+            width: '100%',
+            position: 'absolute',
+            top: '85%',
+            display: 'flex',
+            justifyContent: 'center',
+            paddingLeft: '15px',
+            paddingRight: '15px',
+          }}
+        >
+          <button
+            className="btn btn-primary"
+            style={{
+              fontFamily: 'cursive',
+              fontSize: '20px',
+            }}
+            onClick={() => {
+              console.log('Weblogger: Clicked in button')
+              handlePlayVideo()
+              setIsOpen(false)
+              onclick()
+            }}
+          >
+            {button_name}
+          </button>
+        </div>
+      </Player>,
+    )
+  }
+
+  useEffect(() => {
+    console.log('Weblogger: Iframe fuction has been loaded')
+    console.log('Weblogger: iframe ID ' + document.querySelector('iframe')!.id)
+  }, [])
+
   return (
     <Dialog
       onLoad={async () => {
+        console.log('Weblogger: Dialog loaded')
         const Player = (await import('liqvid')).Player
         const Script = (await import('liqvid')).Script
         const Video = (await import('liqvid')).Video
         const handlePlayVideo = () => {
           vidRef.current!.playback.pause()
         }
-        console.log('Weblogger: Dialog loaded')
-        createRoot(
-          document
-            .querySelector('iframe')!
-            .contentDocument!.querySelector('main')!,
-        ).render(
+        const element = document.querySelector(
+          '#helpIframe',
+        ) as HTMLIFrameElement
+        createRoot(element!.contentDocument!.querySelector('main')!).render(
           <Player id="myPlayer" ref={vidRef} script={new Script(script)}>
             <Video id="myVideo" style={{ aspectRatio: '16/10', width: '100%' }}>
               <source src={mediaURL} type="video/mp4" />
